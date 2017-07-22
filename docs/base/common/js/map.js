39893ffa35eab700a4a73589657f029d35d678e0
HIROASA <hiroasa0605@gmail.com>
2017-07-22 10:32:13 +0900

    osm addr change http -> https

--- docs/base/common/js/map.js
+++ docs/base/common/js/map.js
@@ -51,7 +51,7 @@
 		}).addTo(map);
 
 		L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
-			attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+			attribution: '&copy; <a href="https://osm.org/copyright">OpenStreetMap</a> contributors'
 		}).addTo(map);
 	});
 
